afb315e14cab315023c5747ecd1a112c9d051c30
Chase Stokes <chasehunterstokes@gmail.com>
2022-03-04 08:56:36 -0800

    add Order and Vendor class

---
+++ VendorOrderTracker/Models/Order.cs
@@ -0,0 +1,26 @@
+//Create an Order class. This class should include properties for the title, the description, the price, the date, and any other properties you would like to include.
+
+using System.Collections.Generic;
+
+namespace VendorOrderTracker.Models
+{
+  public class Order
+  {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public string Date { get; set; }
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> {};
+
+    public Order(string title, string description, int price, string date)
+    {
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = date;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+  }
+}
---
+++ VendorOrderTracker/Models/Vendor.cs
@@ -0,0 +1,24 @@
+//Create a Vendor class. This class should include properties for the vendor's name, a description of the vendor, a List of Orders belonging to the vendor, and any other properties you would like to include.
+
+using System.Collections.Generic;
+
+namespace VendorOrderTracker.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
+
+    public Category(string name, string description)
+    {
+      Name = name;
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order>{};
+    }
+  }
+}
