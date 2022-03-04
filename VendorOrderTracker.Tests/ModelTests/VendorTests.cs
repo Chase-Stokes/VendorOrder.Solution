c15472647b7515a45c101e71619e64717c5ced1c
Chase Stokes <chasehunterstokes@gmail.com>
2022-03-04 10:34:25 -0800

    good fail on vendor property test

---
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+    [TestMethod]
+    public void GetProperties_ReturnsProperties_Strings()
+    {
+      string name = "ed";
+      string description = "ed has 2 friends named edd and eddie";
+      Vendor newVendor = new Vendor(name, description);
+      Vendor newVendor2 = new Vendor("naruto", "hes gonna be hokage one day");
+      string nameResult = newVendor2.Name;
+      string descriptionResult = newVendor2.Description;
+      Assert.AreEqual(name, nameResult);
+      Assert.AreEqual(description, descriptionResult);
+    }
+  }
+}
