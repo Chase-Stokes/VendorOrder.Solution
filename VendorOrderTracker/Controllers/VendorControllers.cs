0367c36dc5bf01c354a1834cf85c39620591efca
Chase Stokes <chasehunterstokes@gmail.com>
2022-03-04 11:47:14 -0800

    add controllers

---
+++ VendorOrderTracker/Controllers/VendorControllers.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Controllers
+{
+  public class VendorController : Controller
+  {
+
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string description)
+    {
+      Vendor newVendor = new Vendor(vendorName, description);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
+    
+    [HttpPost("/vendors/{vendorId}/items")]
+    public ActionResult Create(int vendorId, string title, string description, int price, string date)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(title, description, price, date);
+      foundVendor.AddItem(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("items", vendorOrders);
+      model.Add("category", foundVendor);
+      return View("Show", model);
+    }
+
+  }
+}
